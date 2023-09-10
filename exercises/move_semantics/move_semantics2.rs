897973b9398366dc8a388e24c902a23f626e4a92
Psyhackological <konradkon@duck.com>
2023-09-10 17:57:27 +0200

    Update move_semantics to 5.6.0

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -1,51 +1,29 @@
 // move_semantics2.rs
-// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
+//
+// Make the test pass by finding a way to keep both Vecs separate!
+//
+// Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
+// for a hint.
 
-// Expected output:
-// vec0 has length 3 content `[22, 44, 66]`
-// vec1 has length 4 content `[22, 44, 66, 88]`
-
+#[test]
 fn main() {
-    // let vec0 = Vec::new();
-    let mut vec0 = Vec::new();
+    let vec0 = vec![22, 44, 66];
 
-    // Do not move the following line!
-    let mut vec1 = fill_vec_with_mutable_reference(&mut vec0);
-    // let mut vec1 = fill_vec(vec0.clone());
-    // let mut vec1 = fill_vec_with_reference(&vec0);
+    // Create a separate, owned version of vec0 data
+    // let vec0_separate = vec![22, 44, 66];
+    //
+    // let mut vec1 = fill_vec(vec0_separate);
 
-    // Do not change the following line!
-    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
-    vec1.push(88);
-
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    assert_eq!(vec0, vec![22, 44, 66]);
+    assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    vec.push(88);
 
     vec
-}
-
-fn fill_vec_with_reference(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec = vec.clone();
-
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
-
-    vec
-}
-
-fn fill_vec_with_mutable_reference(vec: &mut Vec<i32>) -> Vec<i32> {
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
-
-    vec.clone()
 }
